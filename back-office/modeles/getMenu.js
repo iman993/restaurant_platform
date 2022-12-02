f448941a0374b2b63a9a59d1e768a3f6bfb3e8b0
iman993 <imanemazani@gmail.com>
2022-12-02 20:35:30 +0100

    back-office getion menu add delete update first version

---
+++ back-office/modeles/getMenu.js
@@ -0,0 +1,50 @@
+var mysql = require("mysql");
+
+
+var connexionMySQL = mysql.createConnection({ 
+   host     : "localhost", 
+   user     : "root", 
+   password : "", 
+   database : "jpatest"
+}); 
+var requeteSQL = "SELECT Id,Name,Description,Src,Prix from plates";
+
+var save=function(plate){
+	
+	var sql = `INSERT INTO plates(Name,Description,Src,Prix) VALUES (?, ?,?,?)`;
+  connexionMySQL.query(sql, [plate.Name,plate.Description,plate.Src,plate.Prix],function(err, result) {
+    if (err) throw err;
+    console.log('record inserted');
+  });
+
+}
+var DeleteId=function(req,res){
+	
+	connexionMySQL.query("DELETE FROM Plates WHERE Id = " + req.params.id,
+    function (err, result) {
+      if (err) {
+        res.redirect('/')
+      } else {
+        res.redirect('/')
+      }
+    }
+  ) 	
+}
+
+var getALL=function(resolve, reject){
+        
+          connexionMySQL.query(requeteSQL, function (err, result, fields) {
+            if (err) throw err;
+              resolve(result);
+          });
+        
+    };
+	
+var to_export={
+	Delete:DeleteId,
+	GetAll:getALL,
+	save:save
+};
+
+module.exports = to_export
+
